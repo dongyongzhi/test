4c07e835d1a827f101a335a9a6011b4779cceeb7
dyz <dongyongzhi@foxmail.com>
2020-12-24 09:46:46 +0800

    Initial version of test generated by JHipster-6.10.5

---
+++ src/main/java/com/svein/test/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.svein.test.config.audit;
---
+++ src/main/java/com/svein/test/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.svein.test.repository;
+
+import com.svein.test.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
